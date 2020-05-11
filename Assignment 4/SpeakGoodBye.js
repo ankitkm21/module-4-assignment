10a951440a1f50ad688cf7965820a3ba34ec067e
ankitkm21 <30433288+ankitkm21@users.noreply.github.com>
2020-05-11 19:26:02 +0530

    Add files via upload

---
+++ Assignment 4/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function(window){
+  
+  var speakWord = "Good Bye";
+
+  var byeSpeaker={};
+  byeSpeaker.speak=function(name) {
+    console.log(speakWord + " " + name);
+  }
+  //Expose byeSpeaker to global scope
+  window.byeSpeaker=byeSpeaker;
+})(window);
---
+++ Assignment 4/SpeakHello.js
@@ -0,0 +1,12 @@
+
+(function (window) {
+  
+  var speakWord = "Hello";  
+
+  var helloSpeaker={};
+  helloSpeaker.speak=function(name){
+  console.log(speakWord + " " + name);
+  }
+  //Expose helloSpeaker to global scope
+  window.helloSpeaker=helloSpeaker;
+})(window);
